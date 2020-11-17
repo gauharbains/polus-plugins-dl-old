3de1b6af8ec2cf4bff25de80926d9b0d26dd87f6
Nicholas-Schaub <nick.schaub@axleinfo.com>
2020-11-17 10:26:21 -0500

    Fixed model script

--- polus-cell-nuclei-segmentation/src/get_models.sh
+++ polus-cell-nuclei-segmentation/src/get_models.sh
@@ -4,6 +4,9 @@ unzip dsb2018_topcoders.zip albu/weights/* -d ./dsb2018_topcoders/
 unzip dsb2018_topcoders.zip selim/nn_models/* -d ./dsb2018_topcoders/
 unzip dsb2018_topcoders.zip victor/nn_models/* -d ./dsb2018_topcoders/
 unzip dsb2018_topcoders.zip victor/lgbm_models/* -d ./dsb2018_topcoders/
-unzip dsb2018_topcoders.zip albu/data/folds.csv -d ./dsb2018_topcoders/
+unzip dsb2018_topcoders.zip data/folds.csv -d ./dsb2018_topcoders/
 
-rm dsb2018_topcoders.zip
+mkdir dsb2018_topcoders/data_test
+mkdir dsb2018_topcoders/predictions
+
+# rm dsb2018_topcoders.zip
