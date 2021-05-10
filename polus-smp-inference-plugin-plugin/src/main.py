6a29730b8920a5d5409518f5f5fa997b86d8b75c
gauharbains <gauharbains17@gmail.com>
2021-05-10 17:29:07 -0400

    initial version

--- polus-smp-inference-plugin-plugin/src/main.py
+++ polus-smp-inference-plugin-plugin/src/main.py
@@ -1,10 +1,12 @@
-from bfio.bfio import BioReader, BioWriter
-import argparse, logging, subprocess, time, multiprocessing, sys, os
+from bfio import BioReader, BioWriter
+import argparse, logging, subprocess, time, multiprocessing, sys, os, typing
+import traceback
 import numpy as np
 from pathlib import Path
 import torch 
 import torchvision
 from preprocess import LocalNorm
+import filepattern
 
 tile_size = 1024
 
@@ -28,7 +30,7 @@ if __name__=="__main__":
     parser = argparse.ArgumentParser(prog='main', description='Segmentation models inference plugin')
     
     # Input arguments
-    parser.add_argument('--filePattern', dest='filePattern', type=str,
+    parser.add_argument('--pattern', dest='pattern', type=str,
                         help='Filename pattern used to separate data', required=True)
     parser.add_argument('--inpDir', dest='inpDir', type=str,
                         help='Input image collection to be processed by this plugin', required=True)
@@ -41,8 +43,8 @@ if __name__=="__main__":
     
     # Parse the arguments
     args = parser.parse_args()
-    filePattern = args.filePattern
-    logger.info('filePattern = {}'.format(filePattern))
+    pattern = args.pattern
+    logger.info('pattern = {}'.format(pattern))
     pretrainedModel = args.pretrainedModel
     logger.info('pretrainedModel = {}'.format(pretrainedModel))
     inpDir = args.inpDir
@@ -54,7 +56,8 @@ if __name__=="__main__":
     logger.info('outDir = {}'.format(outDir))
 
     # pretrained models
-    nuclei_model_path = ''
+    cwd = os.getcwd()
+    nuclei_model_path = os.path.join(cwd,'Models','nuclei.pth')
     cyto_model_path = ''
     model_path = nuclei_model_path if pretrainedModel=='Nuclei' else cyto_model_path
 
@@ -70,14 +73,16 @@ if __name__=="__main__":
         model = torch.load(model_path)
         model.eval()
 
-        fp = filePattern.FilePattern(file_path=inpDir, pattern=filePattern)
+        fp = filepattern.FilePattern(file_path=inpDir, pattern=pattern)
          
         # Loop through files in inpDir image collection and process
         for f in fp():
             file_name = f[0]['file']
+            logger.info('Processing image: {}'.format(file_name))
 
             with BioReader(file_name) as br, \
-                BioWriter(Path(outDir).joinpath(Path(file_name).name):
+                BioWriter(Path(outDir).joinpath(Path(file_name).name)):
+                bw.dtype = np.uint8
                 
                 # iterate over tiles
                 for x in range(0,br.X,tile_size):
@@ -85,8 +90,6 @@ if __name__=="__main__":
 
                     for y in range(0,br.Y,tile_size):
                         y_max = min([br.Y,y+tile_size])
-
-                        # load image tile
                         img = br[y:y_max,x:x_max,0:1,0,0][:,:,0,0,0]
 
                         # pad image if required
@@ -98,8 +101,17 @@ if __name__=="__main__":
                         img = preprocess(img).unsqueeze(0)
 
                         with torch.no_grad():
-                            out = model(img)
-            
+                            out = model(img).numpy()
+                        
+                        # postpreocessing and write tile
+                        out[out>0] = 255
+                        out[out<=0] = 0
+                        out = out[0,0,:-pad_dims[0],:-pad_dims[1]]
+                        bw[y:y_max,x:x_max,0:1,0,0] = out.astype(np.uint8)
+
+    except Exception:
+        traceback.print_exc()
+  
     finally:
         logger.info('Finished Execution')
         # Exit the program
--- polus-smp-inference-plugin-plugin/src/preprocess.py
+++ polus-smp-inference-plugin-plugin/src/preprocess.py
@@ -1,4 +1,6 @@
 import numpy as np
+import typing
+import torchvision
 
 class LocalNorm(object):
     def __init__(self,
