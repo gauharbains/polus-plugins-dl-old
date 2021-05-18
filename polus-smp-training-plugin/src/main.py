d0ca0eb5d104a4da8b7ca920cecb1be97816e461
gauharbains <gauharbains17@gmail.com>
2021-05-18 13:14:10 -0400

    added validation

--- polus-smp-training-plugin/src/main.py
+++ polus-smp-training-plugin/src/main.py
@@ -1,10 +1,13 @@
 import argparse, logging, subprocess, time, multiprocessing, sys, traceback
+from os import name
 import numpy as np
 import torch 
 import segmentation_models_pytorch as smp
+import filepattern
+import utils.dataloader as dl
+from sklearn.model_selection import train_test_split
 from pathlib import Path
 from bfio.bfio import BioReader, BioWriter
-from utils.dataloader import Dataset
 from torch.utils.data import DataLoader
 from utils.params import models_dict,loss_dict, metric_dict
 from segmentation_models_pytorch.utils.base import Activation
@@ -82,10 +85,28 @@ if __name__=="__main__":
 
     # Surround with try/finally for proper error catching
     try:
-        logger.info('Initializing dataloader')
+        images_fp = filepattern.FilePattern(file_path=imagesDir, pattern=imagesPattern)
+        labels_fp = filepattern.FilePattern(file_path=labelsDir, pattern=labelsPattern)
+        
+        # map images to corresponding masks
+        name_map = dl.get_image_labels_mapping(images_fp, labels_fp)
+
+        # train validation split
+        image_names = list(name_map.keys())
+        train_names, val_names = train_test_split(image_names, shuffle=True, test_size=0.3)
+        train_map = {k:name_map[k] for k in train_names}
+        val_map = {k:name_map[k] for k in val_names}
+
+        # get tile maps
+        train_tile_map = dl.get_tile_mapping(train_names)
+        val_tile_map = dl.get_tile_mapping(val_names)
+
         # initialize datalaoder
-        dataset = Dataset(imagesDir, labelsDir, imagesPattern, labelsPattern)
-        train_loader = DataLoader(dataset, batch_size=batchSize)
+        logger.info('Initializing dataloader')
+        train_data = dl.Dataset(train_map, train_tile_map)
+        val_data = dl.Dataset(val_map, val_tile_map)
+        train_loader = DataLoader(train_data, batch_size=batchSize)
+        val_loader = DataLoader(val_data, batch_size=batchSize)
 
         # intiliaze model and training parameters
         logger.info('Initializing model,loss,metric')
@@ -106,7 +127,7 @@ if __name__=="__main__":
         dict(params=model.parameters(), lr=0.0001),
         ])
 
-        # training iterator
+        # train and val iterator
         train_epoch = smp.utils.train.TrainEpoch(
             model, 
             loss=loss_class, 
@@ -115,10 +136,18 @@ if __name__=="__main__":
             verbose=True
         )
 
+        valid_epoch = smp.utils.train.ValidEpoch(
+            model, 
+            loss=loss_class, 
+            metrics=metric_class, 
+            verbose=True,
+)
+
         # train and save model
         for i in range(0, epochs):
             logger.info('Epoch: {}'.format(i))
             train_logs = train_epoch.run(train_loader)
+            valid_logs = valid_epoch.run(val_loader)
 
         # save model
         logger.info('saving model...')
--- polus-smp-training-plugin/src/utils/dataloader.py
+++ polus-smp-training-plugin/src/utils/dataloader.py
@@ -8,83 +8,80 @@ import segmentation_models_pytorch as smp
 from bfio import BioReader, BioWriter
 from torch.utils.data import DataLoader
 from torch.utils.data import Dataset as BaseDataset
+from utils.preprocess import LocalNorm
 
-class LocalNorm(object):
-    def __init__(self,
-                 window: int = 129,
-                 clip: typing.Union[int,float] = 6):
-        assert window % 2 == 1, 'window_size must be an odd integer'
-        self.window = window
-        self.clip = clip
-        self.pad = torchvision.transforms.Pad(window//2+1,
-                                              padding_mode='reflect')
-        # Mode can be test, train, and eval
-        self.mode = 'eval'
+tile_size = 256
+
+def get_image_labels_mapping(images_fp, labels_fp):
+    """creates a filename map between images and labels
+    In the case where image filenames have different filename 
+    pattern than label filenames, this function creates a map
+    between the corresponding images and labels
+    
+
+    Args:
+        images_fp ([FilePattern object]): filepattern object for images
+        labels_fp ([FilePattern object]): filepattern object for labels
+
+    Returns:
+        dict: dictionary containing mapping b/w image & label names
+    """
+    name_map = {}
+
+    for f in images_fp():
+        image_name = f[0]['file']
+        vars = {k.upper():v for k,v in f[0].items() if k!='file' }
+        label_name = labels_fp.get_matching(**vars)[0]['file']
+        name_map[image_name] = label_name
+    return name_map
 
 
-    def __call__(self,x):
-        image = self.pad(x)
-        response = self.local_response(image)
-        response[response < -self.clip] = -self.clip
-        response[response > self.clip] = self.clip
-        return response
+def get_tile_mapping(image_names):
+    """ creates a tile map for the Dataset class
+    This function iterates over all the files in the input 
+    collection and creates a dictionary that can be used in 
+    __getitem__ function in the Dataset class. 
+    
 
+    Args:
+        image_names (list): list containing path to images
+    Returns:
+        dict: dictionary containing tile mapping
+    """
+    tile_map = {}
+    tile_num = 0
 
-    def imboxfilt(self,image):
-        """imboxfilt Use a box filter on a stack of images
-        This method applies a box filter to an image. The input is assumed to be a
-        4D array, and should be pre-padded. The output will be smaller by
-        window_size-1 pixels in both width and height since this filter does not pad
-        the input to account for filtering.
-        """
-        # Generate an integral image
-        image_ii = image.cumsum(-1).cumsum(-2)
-        # Create the output
-        output = image_ii[...,0:-self.window-1,0:-self.window-1] + \
-                 image_ii[...,self.window:-1,self.window:-1] - \
-                 image_ii[...,self.window:-1,0:-self.window-1] - \
-                 image_ii[...,0:-self.window-1,self.window:-1]
-        return output
-
+    # iterate over all files
+    for file_name in image_names:
         
-    def local_response(self,image):
-        """local_response Regional normalization
-        This method normalizes each pixel using the mean and standard deviation of
-        all pixels within the window_size. The window_size parameter should be
-        2*radius+1 of the desired region of pixels to normalize by. The image should
-        be padded by window_size//2 on each side.
-        """
-        local_mean = self.imboxfilt(image)/(self.window ** 2)
-        local_mean_square = self.imboxfilt(image.pow(2))/(self.window ** 2)
-        # Use absolute difference because sometimes error causes negative values
-        local_std = (local_mean_square - local_mean.pow(2)).abs().sqrt()
-        local_std[local_std<10**-3] = 10**-3
-        response = (image[...,
-                          self.window//2:-self.window//2-1,
-                          self.window//2:-self.window//2-1] - local_mean)/local_std
-        return response
+        with BioReader(file_name) as br:
+            # iterate over tiles
+            for x in range(0,br.X,tile_size):
+                x_max = min([br.X,x+tile_size])
+                for y in range(0,br.Y, tile_size):
+                    y_max = min([br.Y,y+tile_size])
+
+                    # add tile to tile_map
+                    tile_map[tile_num] = (file_name, (x,x_max), (y,y_max))
+                    tile_num+=1
+
+    return tile_map
+
 
 class Dataset(BaseDataset):
     
-    def __init__(
-            self, 
-            images_dir, 
-            labels_dir,
-            images_pattern,
-            labels_pattern
+    def __init__(self,
+        image_label_map,
+        tile_map
     ):
 
-        self.tile_size = 256
-        self.images_fp = filepattern.FilePattern(file_path=images_dir, pattern=images_pattern)
-        self.labels_fp = filepattern.FilePattern(file_path=labels_dir, pattern=labels_pattern)
-        self.get_tile_mapping()
-        self.get_image_labels_mapping()
+        self.image_label_map = image_label_map
+        self.tile_map = tile_map
         self.preprocessing = torchvision.transforms.Compose([
                              torchvision.transforms.ToTensor(),
                              LocalNorm()])
 
     def __getitem__(self, i):
-        
         
         image_name = self.tile_map[i][0]
         x, x_max = self.tile_map[i][1]
@@ -97,7 +94,7 @@ class Dataset(BaseDataset):
         img = self.preprocessing(img).numpy()
 
         # read and preprocess label
-        label_name = self.name_map[image_name]
+        label_name = self.image_label_map[image_name]
         with BioReader(label_name) as br:
             label = br[y:y_max,x:x_max,0:1,0,0][:,:,0,0,0]      
         label[label>=1] = 1
@@ -110,44 +107,7 @@ class Dataset(BaseDataset):
     def __len__(self):
         return len(self.tile_map.keys())
     
-    def get_tile_mapping(self):
-        """ creates a tile map for the __getitem__ function
-        This function iterates over all the files in the input 
-        collection and creates a dictionary that can be used in 
-        __getitem_ function. 
-        """
-        self.tile_map = {}
-        tile_size = self.tile_size
-        tile_num = 0
 
-        # iterate over all files
-        for f in self.images_fp():
-            file_name = f[0]['file']
-            
-            with BioReader(file_name) as br:
-                # iterate over tiles
-                for x in range(0,br.X,tile_size):
-                    x_max = min([br.X,x+tile_size])
-                    for y in range(0,br.Y, tile_size):
-                        y_max = min([br.Y,y+tile_size])
-
-                        # add tile to tile_map
-                        self.tile_map[tile_num] = (file_name, (x,x_max), (y,y_max))
-                        tile_num+=1
-    
-    def get_image_labels_mapping(self):
-        """creates a filename map between images and labels
-        In the case where image filenames have different filename 
-        pattern than label filenames, this function creates a map
-        between the corresponding images and labels
-        """
-        self.name_map = {}
-
-        for f in self.images_fp():
-            image_name = f[0]['file']
-            vars = {k.upper():v for k,v in f[0].items() if k!='file' }
-            label_name = self.labels_fp.get_matching(**vars)[0]['file']
-            self.name_map[image_name] = label_name
 
 
 
